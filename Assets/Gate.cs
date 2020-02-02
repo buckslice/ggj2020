2d4d3eb4266acede0a34658aa389629f584a3839
Jonathan Odgis <jonathanodgis@gmail.com>
2020-02-02 00:58:17 -0500

    Added in gate object with functionality along with model

---
+++ Assets/Gate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gate : MonoBehaviour
+{
+    public int requiredHappyPeople = 10;
+
+    public enum GateState
+    {
+        LOCKED, UNLOCKING, UNLOCKED
+    }
+
+    public TextMeshProUGUI text;
+
+    Animator anim;
+
+    public GateState gate_state;
+
+    public Collider gateSolidCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gate_state = GateState.LOCKED;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        text.SetText(PlayerInteraction.happyPeople.Count + "/" + requiredHappyPeople);
+    }
+
+    IEnumerator unlockDoor()
+    {
+        gate_state = GateState.UNLOCKING;
+        anim.SetBool("unlock", true);
+        yield return new WaitForSecondsRealtime(1f);
+        gateSolidCollider.enabled = false;
+        gate_state = GateState.UNLOCKED;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If the player has enough of the happy people's open up and be destroyed/unlocked
+        if (PlayerInteraction.happyPeople.Count >= requiredHappyPeople && gate_state == GateState.LOCKED)
+        {
+            StartCoroutine(unlockDoor());
+        }
+    }
+
+}
---
+++ Assets/Script.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public enum GateState
+    {
+        LOCKED, UNLOCKING, UNLOCKED
+    }
+
+    public GateState gate_state = GateState.LOCKED;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gate_state == GateState.LOCKED)
+        {
+            //if the player goes up to it, make a sound error sound
+        }
+        else if (gate_state == GateState.UNLOCKING)
+        {
+            //if the player unlocing,
+        }
+        else if (gate_state == GateState.UNLOCKED)
+        {
+            //if the 
+        }
+    }
+
+    void lockDoor() { }
+
+    void unlockDoor()
+    {
+
+    }
+    
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If the player has enough of the happy people's open up and be destroyed/unlocked
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+    }
+
+    //TO
+
+
+    
+}
