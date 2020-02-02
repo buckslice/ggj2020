1f752603aa06d3324437fbcae83827cc053e0b37
jfc3 <buckslice@gmail.com>
2020-02-02 11:09:49 -0800

    adding canvas fader

---
+++ Assets/Scripts/CanvasFader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasFader : MonoBehaviour {
+
+    public Image background;
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(FadeOut(3.0f));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator FadeOut(float time) {
+        yield return new WaitForSeconds(1);
+        float t = 0.0f;
+        var c = background.color;
+        while (t < time) {
+            t += Time.deltaTime;
+            c.a = t / time;
+            background.color = c;
+            yield return null;
+        }
+    }
+}
